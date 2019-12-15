e3c5d725df65e58e5a19e05c873494e6f761821e
ghd075 <rlfls54@gmail.com>
2019-12-15 23:24:21 +0900

    3.4 커넥션 풀 설정!!

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -1,10 +1,27 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zerock"})
 public class RootConfig {
-
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		
+		return new HikariDataSource(hikariConfig);
+	}
+	
 }
