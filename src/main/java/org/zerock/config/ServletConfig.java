107df3c2cf42e16513fdabc87b731570785d18ae
ghd075 <rlfls54@gmail.com>
2019-12-23 22:35:03 +0900

    6.6.1 @ControllerAdvice 에러처리!!

--- src/main/java/org/zerock/config/ServletConfig.java
+++ src/main/java/org/zerock/config/ServletConfig.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 //서블릿 관련 설정
 @EnableWebMvc
-@ComponentScan(basePackages = {"org.zerock.controller"})
+@ComponentScan(basePackages = {"org.zerock.controller", "org.zerock.exception"})
 public class ServletConfig implements WebMvcConfigurer {
 
 	@Override
