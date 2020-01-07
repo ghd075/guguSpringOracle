76e4b196201879714d5cad72d04c8d7ebef83710
ghd075 <rlfls54@gmail.com>
2020-01-07 13:38:46 +0900

    16.2.2 객체의 반환

--- src/main/java/org/zerock/controller/SampleRestController.java
+++ src/main/java/org/zerock/controller/SampleRestController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.SampleVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -19,5 +20,16 @@ public class SampleRestController {
 		
 		return "안녕하세요";
 	}
+	
+    @GetMapping(value = "/getSample", 
+    		produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, 
+    					MediaType.APPLICATION_XML_VALUE})
+    public SampleVO getSample() {
+        return new SampleVO(112, "스타", "로드");
+    }
 
+    @GetMapping(value = "/getSample2")
+    public SampleVO getSample2() {
+        return new SampleVO(113, "로켓", "라쿤");
+    }
 }
