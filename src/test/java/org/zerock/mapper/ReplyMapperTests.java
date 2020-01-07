989e1a60eb9757bf7a7aa0d51aa730571d354d10
ghd075 <rlfls54@gmail.com>
2020-01-07 17:13:09 +0900

    17.2.4 @Param어노테이션과 댓글 목록록

--- src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -1,5 +1,6 @@
 package org.zerock.mapper;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.config.RootConfig;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 
 import lombok.Setter;
@@ -70,6 +72,17 @@ public class ReplyMapperTests {
 		int count = mapper.update(vo);
 		
 		log.info("UPDATE COUNT : " + count);
+	}
+	
+	@Test
+	public void testList() {
+		
+		Criteria cri = new Criteria();
+		
+		// 11L
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
+		
+		replies.forEach(reply -> log.info(reply));
 	}
 	
 	@Test
