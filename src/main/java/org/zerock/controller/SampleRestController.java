eda42a185374a7d0d65654ae5c2a3e5a233002ab
ghd075 <rlfls54@gmail.com>
2020-01-07 13:04:24 +0900

    16.2.1 단순 문자열 반환

--- src/main/java/org/zerock/controller/SampleRestController.java
+++ src/main/java/org/zerock/controller/SampleRestController.java
@@ -1,5 +1,7 @@
 package org.zerock.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,12 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleRestController {
 	
+	@GetMapping(value = "/getTest", produces = "text/plain; charset=UTF-8")
+	public String getTest() {
+		
+		log.info("MIME TYPE : " + MediaType.TEXT_PLAIN);
+		
+		return "안녕하세요";
+	}
 
 }
